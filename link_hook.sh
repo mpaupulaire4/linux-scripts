3518dba294a23e697b96c12485bf1fc1db64868f
Michel Pupulaire <mpaupulaire4@gmail.com>
2021-09-29 13:40:37 -0400

    save

--- link_hook.sh
+++ link_hook.sh
@@ -4,13 +4,13 @@ source ./utils.sh
 
 set -e
 
-hook_dir = '/etc/pacman.d/hooks'
+HOOK_DIR='/etc/pacman.d/hooks'
 
-echo "Set HookDir to $hook_dir/"
+echo "Set HookDir to $HOOK_DIR/"
 sudo nan /etc/pacman.conf
 
-mkdir -p $hook_dir
-cp -i $MCONF/pkglists.hook $hook_dir/
+mkdir -p $HOOK_DIR
+cp -i $MCONF/pkglists.hook $HOOK_DIR/
 
 create_link "$MCONF/pkglist.txt" "/etc/pkglist.txt"
 create_link "$MCONF/fpkglist.txt" "/etc/fpkglist.txt"
