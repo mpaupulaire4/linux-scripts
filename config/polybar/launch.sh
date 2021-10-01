c9648e3eb415433d80761fe3b3ced75ad254bd7a
Michel Pupulaire <mpaupulaire4@gmail.com>
2021-10-01 14:41:33 -0400

    save

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar-top.log
-polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar-top.log & disown
 
 echo "Bar launched..."
