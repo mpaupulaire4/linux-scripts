150a77b5ffd2e1f5e3c6b52b688d40fef7ce57da
Michel Pupulaire <mpaupulaire4@gmail.com>
2021-10-01 14:36:01 -0400

    save

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,11 +2,11 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "---" | tee -a /tmp/polybar-top.log
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bar launched..."
